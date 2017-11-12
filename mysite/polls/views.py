7405dca22ff7638185a04030c1c65eb09b1d062d
Anton Sankov <anton.sankov@sap.com>
2017-11-12 19:43:10 +0200

    Added context_object_name in views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,18 +2,33 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Returns the last five published questions (does not include those set to be published in the future)
+        """
+
+        return Question.objects.filter(
+            pub_date__lte = timezone.now()
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/details.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that are not published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
